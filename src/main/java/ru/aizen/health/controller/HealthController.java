b56ffda52ba08534d6d09a3b423a5c93ebbd580c
Aizen <symryvvin@gmail.com>
2021-01-27 21:41:34 +0300

    feat: add /health get request media type

--- src/main/java/ru/aizen/health/controller/HealthController.java
+++ src/main/java/ru/aizen/health/controller/HealthController.java
@@ -1,12 +1,18 @@
 package ru.aizen.health.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthController {
 
-	@GetMapping("/health")
+
+	@RequestMapping(value = "/health",
+			method = RequestMethod.GET,
+			produces = {MediaType.APPLICATION_JSON_VALUE}
+	)
 	public HealthResponse health() {
 		return new HealthResponse("OK");
 	}
